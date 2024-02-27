66050c4998d321a5ff029a2a78706de22f34db5d
Oleg Riabush <ryabush.oleg@gmail.com>
2024-02-27 15:18:23 +0200

    main

---
+++ main.tf
@@ -0,0 +1,7 @@
+module "gke_cluster" {
+  source         = "github.com/vikingoleg/tf-google-gke-cluster"
+  GOOGLE_REGION  = var.GOOGLE_REGION
+  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  GKE_NUM_NODES  = 2
+}
+  
